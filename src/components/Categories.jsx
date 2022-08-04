62834e44ca781d552473ed8dc43723f168a5fd28
koltrastH <elina.hulbert@hyperisland.se>
2022-08-04 14:26:01 +0200

    toggle added to categories, moved img folder to src

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li className={activeIndex === 0 ? "active" : ""}>Все</li>
+        <li className={activeIndex === 1 ? "active" : ""}>Мясные</li>
+        <li className={activeIndex === 2 ? "active" : ""}>Вегетарианская</li>
+        <li className={activeIndex === 3 ? "active" : ""}>Гриль</li>
+        <li className={activeIndex === 4 ? "active" : ""}>Острые</li>
+        <li className={activeIndex === 5 ? "active" : ""}>Закрытые</li>
       </ul>
     </div>
   );
