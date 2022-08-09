975dbc0adc3e863869da2f13e118d10ab8251910
koltrastH <elina.hulbert@hyperisland.se>
2022-08-09 16:22:26 +0200

    connected css modules, created NotFound page&component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 // import pizzas from "./assets/pizzas.json";
 import "./scss/app.scss";
@@ -12,7 +13,7 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <NotFound />
         </div>
       </div>
     </div>
---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./NotFoundBlock.module.scss";
+
+const NotFoundBlock = () => {
+  return (
+    <h1 className={styles.root}>
+      <span>😕</span>
+      <br />
+      404 Error Not Found
+    </h1>
+  );
+};
+
+export default NotFoundBlock;
