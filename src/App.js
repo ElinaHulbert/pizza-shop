3f505d70237b23f94f386dc1c781b1e9310da679
koltrastH <elina.hulbert@hyperisland.se>
2022-08-10 22:28:17 +0200

    Routes and Links fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 
 // import pizzas from "./assets/pizzas.json";
@@ -13,7 +14,11 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <NotFound />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
