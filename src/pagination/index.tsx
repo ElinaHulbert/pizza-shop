f029ab6959846192c987bd071e70a4e9fcc2a192
koltrastH <elina.hulbert@hyperisland.se>
2023-02-21 19:44:04 +0100

    formatting

--- src/pagination/index.tsx
+++ src/pagination/index.tsx
@@ -4,22 +4,18 @@ import ReactPaginate from "react-paginate";
 
 type PaginationProps = {
   onChangePage: (page: number) => void;
-  
-}
+};
 
-const Pagination: React.FC<PaginationProps> = ({ onChangePage }) => {
-  
-  return (
-    <ReactPaginate
-      className={styles.root}
-      breakLabel="..."
-      nextLabel=">"
-      previousLabel="<"
-      onPageChange={(event) => onChangePage(event.selected + 1)}
-      pageRangeDisplayed={4}
-      pageCount={3}
-    />
-  );
-}
+const Pagination: React.FC<PaginationProps> = ({ onChangePage }) => (
+  <ReactPaginate
+    className={styles.root}
+    breakLabel="..."
+    nextLabel=">"
+    previousLabel="<"
+    onPageChange={(event) => onChangePage(event.selected + 1)}
+    pageRangeDisplayed={4}
+    pageCount={3}
+  />
+);
 
 export default Pagination;
