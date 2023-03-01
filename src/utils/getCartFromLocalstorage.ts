a3971e6754c40f96bf1acc80ea4590a2407ea355
Elina Hulbert <elina.hulbert@hyperisland.se>
2023-03-01 13:10:21 +0100

    import fix

--- src/utils/getCartFromLocalstorage.ts
+++ src/utils/getCartFromLocalstorage.ts
@@ -1,11 +1,11 @@
-import  {CartItem}  from "../redux/cart/types";
-import { calcTotalPrice } from './calcTotalPrice';
+import { CartItem } from "../redux/cart/types";
+import { calcTotalPrice } from "./calcTotalPrice";
 export const getCartFromLocalstorage = () => {
-    const data = localStorage.getItem("cart");
-    const items = data ? JSON.parse(data)  : [];
-    const totalPrice = calcTotalPrice(items)
-    return {
-        items: items as CartItem[],
-        totalPrice,
-    }
-}
+  const data = localStorage.getItem("cart");
+  const items = data ? JSON.parse(data) : [];
+  const totalPrice = calcTotalPrice(items);
+  return {
+    items: items as CartItem[],
+    totalPrice,
+  };
+};
